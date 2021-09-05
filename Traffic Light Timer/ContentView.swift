40b8daf307bd65dcb6cb6cad75e2c64f5f04de5d
pradyun <pradyun.setti@gmail.com>
2021-09-05 15:56:13 +1200

    Initial Commit

---
+++ Traffic Light Timer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Traffic Light Timer
+//
+//  Created by Pradyun Setti on 5/09/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Traffic Light Timer/Traffic_Light_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Traffic_Light_TimerApp.swift
+//  Traffic Light Timer
+//
+//  Created by Pradyun Setti on 5/09/21.
+//
+
+import SwiftUI
+
+@main
+struct Traffic_Light_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
